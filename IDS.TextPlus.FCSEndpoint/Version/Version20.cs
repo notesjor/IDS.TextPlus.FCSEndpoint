b6aebb849c4a3a2110c05c8e03105bf8865c7ba0
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2024-02-03 13:20:45 +0100

    // commit

--- IDS.TextPlus.FCSEndpoint/Version/Version20.cs
+++ IDS.TextPlus.FCSEndpoint/Version/Version20.cs
@@ -12,6 +12,8 @@ namespace IDS.TextPlus.FCSEndpoint.Version
   public class Version20 : AbstractVersion
   {
     private string _mime = "application/xml;charset=utf-8";
+    private int _maxRecords = 1000;
+
     private string DefaultRouteResponse;
     private string EndpointDescriptionResponse;
     private string Template_Error_RecordXmlEscaping;
@@ -20,7 +22,7 @@ namespace IDS.TextPlus.FCSEndpoint.Version
 
     public Version20()
     {
-      DefaultRouteResponse = System.IO.File.ReadAllText("Snippets/20DefaultRoute.xml", Encoding.UTF8);
+      DefaultRouteResponse = System.IO.File.ReadAllText("Snippets/20DefaultRoute.xml", Encoding.UTF8).Replace("{{max}}", _maxRecords.ToString());
       EndpointDescriptionResponse = System.IO.File.ReadAllText("Snippets/20EndpointDescription.xml", Encoding.UTF8);
       Template_Error_RecordXmlEscaping = System.IO.File.ReadAllText("Snippets/20Template_Error_recordXMLEscaping.xml", Encoding.UTF8);
       Template_Error_Number = System.IO.File.ReadAllText("Snippets/20Template_Error_Number.xml", Encoding.UTF8);
@@ -30,18 +32,18 @@ namespace IDS.TextPlus.FCSEndpoint.Version
     public override void ProcessRequest(HttpContext ctx)
     {
       var data = ctx.GetData();
-      if(data.ContainsKey("x-fcs-endpoint-description") )
+      if (data.ContainsKey("x-fcs-endpoint-description"))
       {
         ctx.Response.Send(EndpointDescriptionResponse, _mime);
         return;
       }
 
       GetUrlParameterNumber(ctx, ref data, "startrecord", "startRecord", 1, 1, out var start, Template_Error_Number);
-      GetUrlParameterNumber(ctx, ref data, "responseposition", "responsePosition", 1, 1, out start, Template_Error_ScanNumber); // startRecord = responsePosition
-      GetUrlParameterNumber(ctx, ref data, "maximumrecords", "maximumRecords", 10, 0, 250, out var maximum, Template_Error_Number);
-      GetUrlParameterNumber(ctx, ref data, "maximumterms", "maximumTerms", 10, 0, 250, out maximum, Template_Error_ScanNumber); // maximumRecords = maximumTerms
+      GetUrlParameterNumber(ctx, ref data, "responseposition", "responsePosition", start, 1, out start, Template_Error_ScanNumber); // startRecord = responsePosition
+      GetUrlParameterNumber(ctx, ref data, "maximumrecords", "maximumRecords", 10, 0, _maxRecords, out var maximum, Template_Error_Number);
+      GetUrlParameterNumber(ctx, ref data, "maximumterms", "maximumTerms", maximum, 0, _maxRecords, out maximum, Template_Error_ScanNumber); // maximumRecords = maximumTerms
 
-      var recordXMLEscaping = ""; // stupid parameter, but it's in the requirements
+      var recordXMLEscaping = ""; 
       if (data.ContainsKey("recordxmlescaping"))
       {
         var escaping = data["recordxmlescaping"];
