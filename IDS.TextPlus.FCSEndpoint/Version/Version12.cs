a280161e22cb5bb3319c07709a1f602ad5c524d6
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2025-01-23 14:42:09 +0100

    // QuerySyntax Error

--- IDS.TextPlus.FCSEndpoint/Version/Version12.cs
+++ IDS.TextPlus.FCSEndpoint/Version/Version12.cs
@@ -1,116 +1,112 @@
-﻿using IDS.TextPlus.FCSEndpoint.Helper;
-using IDS.TextPlus.FCSEndpoint.Model;
+﻿using System.Text;
+using IDS.TextPlus.FCSEndpoint.Helper;
 using IDS.TextPlus.FCSEndpoint.Version.Abstract;
-using Newtonsoft.Json;
-using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using Tfres;
 
-namespace IDS.TextPlus.FCSEndpoint.Version
+namespace IDS.TextPlus.FCSEndpoint.Version;
+
+public class Version12 : AbstractVersion
 {
-  public class Version12 : AbstractVersion
+  private readonly string DefaultRouteResponse;
+  private readonly string EmptyResult;
+  private readonly string EndpointDescriptionResponse;
+  private readonly string Error_OutOfRange;
+  private readonly string Error_QueryParser;
+  private readonly string Template_Error_Number;
+  private readonly string Template_Error_RecordXmlEscaping;
+  private readonly string Template_Error_ScanNumber;
+  private readonly string Template_Response_01;
+  private readonly string Template_Response_02;
+  private readonly string Template_Response_03;
+  private readonly string Template_Response_04;
+  private readonly string Template_Response_05;
+
+  public Version12()
   {
-    private string DefaultRouteResponse;
-    private string EmptyResult;
-    private string EndpointDescriptionResponse;
-    private string Error_OutOfRange;
-    private string Error_QueryParser;
-    private string Template_Error_RecordXmlEscaping;
-    private string Template_Error_Number;
-    private string Template_Error_ScanNumber;
-    private string Template_Response_01;
-    private string Template_Response_02;
-    private string Template_Response_03;
-    private string Template_Response_04;
-    private string Template_Response_05;
+    DefaultRouteResponse = File.ReadAllText("Snippets/12/12DefaultRoute.xml", Encoding.UTF8).Replace("{{max}}", _maxRecords.ToString());
+    EndpointDescriptionResponse = DefaultRouteResponse;
+    Error_QueryParser = File.ReadAllText("Snippets/12/12Error_QueryParser.xml", Encoding.UTF8);
+    Template_Error_RecordXmlEscaping = File.ReadAllText("Snippets/12/12Template_Error_recordPacking.xml", Encoding.UTF8);
+    Template_Error_Number = File.ReadAllText("Snippets/12/12Template_Error_Number.xml", Encoding.UTF8);
+    EmptyResult = File.ReadAllText("Snippets/12/12EmptyResult.xml", Encoding.UTF8);
+    Error_OutOfRange = File.ReadAllText("Snippets/12/12Error_OutOfRange.xml", Encoding.UTF8);
+    Template_Error_ScanNumber = File.ReadAllText("Snippets/12/12Template_Error_Scan_Number.xml", Encoding.UTF8);
+    Template_Response_01 = File.ReadAllText("Snippets/12/12Template_Response_01.xml", Encoding.UTF8);
+    Template_Response_02 = File.ReadAllText("Snippets/12/12Template_Response_02.xml", Encoding.UTF8);
+    Template_Response_03 = File.ReadAllText("Snippets/12/12Template_Response_03.xml", Encoding.UTF8);
+    Template_Response_04 = File.ReadAllText("Snippets/12/12Template_Response_04.xml", Encoding.UTF8);
+    Template_Response_05 = File.ReadAllText("Snippets/12/12Template_Response_05.xml", Encoding.UTF8);
+  }
 
-    public Version12()
+  public override void ProcessRequest(HttpContext ctx, ref Dictionary<string, string> data)
+  {
+    if (data.ContainsKey("x-fcs-endpoint-description"))
     {
-      DefaultRouteResponse = System.IO.File.ReadAllText("Snippets/12/12DefaultRoute.xml", Encoding.UTF8).Replace("{{max}}", _maxRecords.ToString());
-      EndpointDescriptionResponse = DefaultRouteResponse;
-      Error_QueryParser = System.IO.File.ReadAllText("Snippets/12/12Error_QueryParser.xml", Encoding.UTF8);
-      Template_Error_RecordXmlEscaping = System.IO.File.ReadAllText("Snippets/12/12Template_Error_recordPacking.xml", Encoding.UTF8);
-      Template_Error_Number = System.IO.File.ReadAllText("Snippets/12/12Template_Error_Number.xml", Encoding.UTF8);
-      EmptyResult = System.IO.File.ReadAllText("Snippets/12/12EmptyResult.xml", Encoding.UTF8);
-      Error_OutOfRange = System.IO.File.ReadAllText("Snippets/12/12Error_OutOfRange.xml", Encoding.UTF8);
-      Template_Error_ScanNumber = System.IO.File.ReadAllText("Snippets/12/12Template_Error_Scan_Number.xml", Encoding.UTF8);
-      Template_Response_01 = System.IO.File.ReadAllText("Snippets/12/12Template_Response_01.xml", Encoding.UTF8);
-      Template_Response_02 = System.IO.File.ReadAllText("Snippets/12/12Template_Response_02.xml", Encoding.UTF8);
-      Template_Response_03 = System.IO.File.ReadAllText("Snippets/12/12Template_Response_03.xml", Encoding.UTF8);
-      Template_Response_04 = System.IO.File.ReadAllText("Snippets/12/12Template_Response_04.xml", Encoding.UTF8);
-      Template_Response_05 = System.IO.File.ReadAllText("Snippets/12/12Template_Response_05.xml", Encoding.UTF8);
+      ctx.Response.Send(EndpointDescriptionResponse, _mime);
+      return;
     }
 
-    public override void ProcessRequest(HttpContext ctx, ref Dictionary<string, string> data)
+    if (data.ContainsKey("recordpacking") && data["recordpacking"].ToLower() != "xml")
     {
-      if (data.ContainsKey("x-fcs-endpoint-description"))
-      {
-        ctx.Response.Send(EndpointDescriptionResponse, _mime);
-        return;
-      }
-
-      if (data.ContainsKey("recordpacking") && data["recordpacking"].ToLower() != "xml")
-      {
-        ctx.Response.Send(Template_Error_RecordXmlEscaping.Replace("{{format}}", data["recordpacking"]));
-        return;
-      }
-
-      GetUrlParameterNumber(ctx, ref data, "startrecord", "startRecord", 1, 1, out var start, Template_Error_Number);
-      GetUrlParameterNumber(ctx, ref data, "responseposition", "responsePosition", start, 1, out start, Template_Error_ScanNumber); // startRecord = responsePosition
-      GetUrlParameterNumber(ctx, ref data, "maximumrecords", "maximumRecords", 10, 0, _maxRecords, out var maximum, Template_Error_Number);
-      GetUrlParameterNumber(ctx, ref data, "maximumterms", "maximumTerms", maximum, 0, _maxRecords, out maximum, Template_Error_ScanNumber); // maximumRecords = maximumTerms
-
-      if (data.ContainsKey("query"))
-      {
-        ExecuteQuery(ctx, data["query"], start, maximum);
-        return;
-      }
-      if (data.ContainsKey("operation") && data["operation"] == "scan")
-      {
-        ExecuteQuery(ctx, "", start, maximum);
-        return;
-      }
-
-      ctx.Response.Send(DefaultRouteResponse, _mime);
+      ctx.Response.Send(Template_Error_RecordXmlEscaping.Replace("{{format}}", data["recordpacking"]));
+      return;
     }
 
-    private void ExecuteQuery(HttpContext ctx, string query, int start, int maximum)
-    {
-      try
-      {
-        SearchResponse result = Search.Send(query, start, maximum);
+    GetUrlParameterNumber(ctx, ref data, "startrecord", "startRecord", 1, 1, out var start, Template_Error_Number);
+    GetUrlParameterNumber(ctx, ref data, "responseposition", "responsePosition", start, 1, out start, Template_Error_ScanNumber); // startRecord = responsePosition
+    GetUrlParameterNumber(ctx, ref data, "maximumrecords", "maximumRecords", 10, 0, _maxRecords, out var maximum, Template_Error_Number);
+    GetUrlParameterNumber(ctx, ref data, "maximumterms", "maximumTerms", maximum, 0, _maxRecords, out maximum, Template_Error_ScanNumber); // maximumRecords = maximumTerms
 
-        if (result?.Hits == null || result.Hits.Length == 0)
+    if (data.ContainsKey("query"))
+    {
+      ExecuteQuery(ctx, data["query"], start, maximum);
+      return;
+    }
+
+    if (data.ContainsKey("operation") && data["operation"] == "scan")
+    {
+      ExecuteQuery(ctx, "", start, maximum);
+      return;
+    }
+
+    ctx.Response.Send(DefaultRouteResponse, _mime);
+  }
+
+  private void ExecuteQuery(HttpContext ctx, string query, int start, int maximum)
+  {
+    try
+    {
+      var result = Search.Send(query, start, maximum);
+
+      if (result?.Hits == null || result.Hits.Length == 0)
+      {
+        if (start > 1 && start > result?.EstimatedTotalHits)
         {
-          if (start > 1 && start > result?.EstimatedTotalHits)
-          {
-            ctx.Response.Send(Error_OutOfRange, _mime);
-            return;
-          }
-
-          ctx.Response.Send(EmptyResult.Replace("{{query}}", query), _mime);
+          ctx.Response.Send(Error_OutOfRange, _mime);
           return;
         }
 
-        ctx.Response.SendChunk(Template_Response_01, mimeType: _mime);
-        ctx.Response.SendChunk(result.EstimatedTotalHits.ToString());
-        ctx.Response.SendChunk(Template_Response_02);
-
-        for (int i = 0; i < result.Hits.Length; i++)
-          ctx.Response.SendChunk(Template_Response_03.Replace("{{id}}", result.Hits[i].Formatted.Id.ToString()).Replace("{{url}}", result.Hits[i].Formatted.Url).Replace("{{hit}}", result.Hits[i].Formatted.Text).Replace("{{p}}", (result.Offset + i).ToString()));
-
-        ctx.Response.SendChunk(Template_Response_04);
-        ctx.Response.SendFinalChunk(Template_Response_05.Replace("{{query}}", query).Replace("{{start}}", (result.Offset + 1).ToString()).Replace("{{offset}}", start.ToString()).Replace("{{max}}", result.EstimatedTotalHits.ToString()));
+        ctx.Response.Send(EmptyResult.Replace("{{query}}", query), _mime);
+        return;
       }
-      catch (TypeLoadException)
-      {
-        ctx.Response.Send(Error_QueryParser);
-      }
+
+      ctx.Response.SendChunk(Template_Response_01, mimeType: _mime);
+      ctx.Response.SendChunk(result.EstimatedTotalHits.ToString());
+      ctx.Response.SendChunk(Template_Response_02);
+
+      for (var i = 0; i < result.Hits.Length; i++)
+        ctx.Response.SendChunk(Template_Response_03.Replace("{{id}}", result.Hits[i].Formatted.Id)
+          .Replace("{{url}}", result.Hits[i].Formatted.Url).Replace("{{hit}}", result.Hits[i].Formatted.Text)
+          .Replace("{{p}}", (result.Offset + i).ToString()));
+
+      ctx.Response.SendChunk(Template_Response_04);
+      ctx.Response.SendFinalChunk(Template_Response_05.Replace("{{query}}", query)
+        .Replace("{{start}}", (result.Offset + 1).ToString()).Replace("{{offset}}", start.ToString())
+        .Replace("{{max}}", result.EstimatedTotalHits.ToString()));
+    }
+    catch (TypeLoadException)
+    {
+      ctx.Response.Send(Error_QueryParser);
     }
   }
 }
--- IDS.TextPlus.FCSEndpoint/Version/Version20.cs
+++ IDS.TextPlus.FCSEndpoint/Version/Version20.cs
@@ -1,119 +1,124 @@
-﻿using IDS.TextPlus.FCSEndpoint.Helper;
-using IDS.TextPlus.FCSEndpoint.Model;
-using IDS.TextPlus.FCSEndpoint.Version.Abstract;
-using Newtonsoft.Json;
-using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
+using IDS.TextPlus.FCSEndpoint.Helper;
+using IDS.TextPlus.FCSEndpoint.Version.Abstract;
 using Tfres;
 
-namespace IDS.TextPlus.FCSEndpoint.Version
+namespace IDS.TextPlus.FCSEndpoint.Version;
+
+public class Version20 : AbstractVersion
 {
-  public class Version20 : AbstractVersion
+  private readonly string DefaultRouteResponse;
+  private readonly string EmptyResult;
+  private readonly string EndpointDescriptionResponse;
+  private readonly string Error_OutOfRange;
+  private readonly string Error_QueryParser;
+  private readonly string Error_QuerySyntax;
+  private readonly string Template_Error_Number;
+  private readonly string Template_Error_RecordXmlEscaping;
+  private readonly string Template_Error_ScanNumber;
+  private readonly string Template_Response_01;
+  private readonly string Template_Response_02;
+  private readonly string Template_Response_03;
+  private readonly string Template_Response_04;
+  private readonly string Template_Response_05;
+
+  public Version20()
   {
-    private string DefaultRouteResponse;
-    private string EmptyResult;
-    private string EndpointDescriptionResponse;
-    private string Error_OutOfRange;
-    private string Error_QueryParser;
-    private string Template_Error_RecordXmlEscaping;
-    private string Template_Error_Number;
-    private string Template_Error_ScanNumber;
-    private string Template_Response_01;
-    private string Template_Response_02;
-    private string Template_Response_03;
-    private string Template_Response_04;
-    private string Template_Response_05;
+    DefaultRouteResponse = File.ReadAllText("Snippets/20/20DefaultRoute.xml", Encoding.UTF8).Replace("{{max}}", _maxRecords.ToString());
+    EmptyResult = File.ReadAllText("Snippets/20/20EmptyResult.xml", Encoding.UTF8);
+    EndpointDescriptionResponse = File.ReadAllText("Snippets/20/20EndpointDescription.xml", Encoding.UTF8);
 
-    public Version20()
+    Error_OutOfRange = File.ReadAllText("Snippets/20/20Error_OutOfRange.xml", Encoding.UTF8);
+    Error_QueryParser = File.ReadAllText("Snippets/20/20Error_QueryParser.xml", Encoding.UTF8);
+    Error_QuerySyntax = File.ReadAllText("Snippets/20/20Error_QuerySyntax.xml", Encoding.UTF8);
+
+    Template_Error_RecordXmlEscaping = File.ReadAllText("Snippets/20/20Template_Error_recordXMLEscaping.xml", Encoding.UTF8);
+    Template_Error_Number = File.ReadAllText("Snippets/20/20Template_Error_Number.xml", Encoding.UTF8);
+    Template_Error_ScanNumber = File.ReadAllText("Snippets/20/20Template_Error_Scan_Number.xml", Encoding.UTF8);
+
+    Template_Response_01 = File.ReadAllText("Snippets/20/20Template_Response_01.xml", Encoding.UTF8);
+    Template_Response_02 = File.ReadAllText("Snippets/20/20Template_Response_02.xml", Encoding.UTF8);
+    Template_Response_03 = File.ReadAllText("Snippets/20/20Template_Response_03.xml", Encoding.UTF8);
+    Template_Response_04 = File.ReadAllText("Snippets/20/20Template_Response_04.xml", Encoding.UTF8);
+    Template_Response_05 = File.ReadAllText("Snippets/20/20Template_Response_05.xml", Encoding.UTF8);
+  }
+
+  public override void ProcessRequest(HttpContext ctx, ref Dictionary<string, string> data)
+  {
+    if (data.ContainsKey("x-fcs-endpoint-description"))
     {
-      DefaultRouteResponse = System.IO.File.ReadAllText("Snippets/20/20DefaultRoute.xml", Encoding.UTF8).Replace("{{max}}", _maxRecords.ToString());
-      EmptyResult = System.IO.File.ReadAllText("Snippets/20/20EmptyResult.xml", Encoding.UTF8);
-      EndpointDescriptionResponse = System.IO.File.ReadAllText("Snippets/20/20EndpointDescription.xml", Encoding.UTF8);
-      Error_OutOfRange = System.IO.File.ReadAllText("Snippets/20/20Error_OutOfRange.xml", Encoding.UTF8);
-      Error_QueryParser = System.IO.File.ReadAllText("Snippets/20/20Error_QueryParser.xml", Encoding.UTF8);
-      Template_Error_RecordXmlEscaping = System.IO.File.ReadAllText("Snippets/20/20Template_Error_recordXMLEscaping.xml", Encoding.UTF8);
-      Template_Error_Number = System.IO.File.ReadAllText("Snippets/20/20Template_Error_Number.xml", Encoding.UTF8);
-      Template_Error_ScanNumber = System.IO.File.ReadAllText("Snippets/20/20Template_Error_Scan_Number.xml", Encoding.UTF8);
-
-      Template_Response_01 = System.IO.File.ReadAllText("Snippets/20/20Template_Response_01.xml", Encoding.UTF8);
-      Template_Response_02 = System.IO.File.ReadAllText("Snippets/20/20Template_Response_02.xml", Encoding.UTF8);
-      Template_Response_03 = System.IO.File.ReadAllText("Snippets/20/20Template_Response_03.xml", Encoding.UTF8);
-      Template_Response_04 = System.IO.File.ReadAllText("Snippets/20/20Template_Response_04.xml", Encoding.UTF8);
-      Template_Response_05 = System.IO.File.ReadAllText("Snippets/20/20Template_Response_05.xml", Encoding.UTF8);
+      ctx.Response.Send(EndpointDescriptionResponse, _mime);
+      return;
     }
 
-    public override void ProcessRequest(HttpContext ctx, ref Dictionary<string, string> data)
+    if (data.ContainsKey("recordxmlescaping") && data["recordxmlescaping"].ToLower() != "xml")
     {
-      if (data.ContainsKey("x-fcs-endpoint-description"))
+      ctx.Response.Send(Template_Error_RecordXmlEscaping.Replace("{{format}}", data["recordxmlescaping"]));
+      return;
+    }
+
+    GetUrlParameterNumber(ctx, ref data, "startrecord", "startRecord", 1, 1, out var start, Template_Error_Number);
+    GetUrlParameterNumber(ctx, ref data, "maximumrecords", "maximumRecords", 10, 0, _maxRecords, out var maximum, Template_Error_Number);
+    // URL parameters are specified but meaningless. In version 1.2 they are used for the scan - which is not implemented in 2.0. But the parameters must be checked according to the specification.
+    GetUrlParameterNumber(ctx, ref data, "responseposition", "responsePosition", start, 1, out start, Template_Error_ScanNumber); // startRecord = responsePosition
+    GetUrlParameterNumber(ctx, ref data, "maximumterms", "maximumTerms", maximum, 0, _maxRecords, out maximum, Template_Error_ScanNumber); // maximumRecords = maximumTerms
+
+    if (data.ContainsKey("query"))
+      ExecuteQuery(ctx, data["query"], start, maximum);
+    else
+      ctx.Response.Send(DefaultRouteResponse, _mime);
+  }
+
+  private void ExecuteQuery(HttpContext ctx, string query, int start, int maximum)
+  {
+    try
+    {
+      // FCS specs require an error message if the query is empty - but the regular CQL parser does not throw an exception in this case.
+      if (query.EndsWith("="))
       {
-        ctx.Response.Send(EndpointDescriptionResponse, _mime);
+        ctx.Response.Send(Error_QuerySyntax, _mime);
         return;
       }
 
-      if (data.ContainsKey("recordxmlescaping") && data["recordxmlescaping"].ToLower() != "xml")
+      var result = Search.Send(query, start, maximum);
+
+      if (result?.Hits == null || result.Hits.Length == 0)
       {
-        ctx.Response.Send(Template_Error_RecordXmlEscaping.Replace("{{format}}", data["recordxmlescaping"]));
-        return;
-      }
-
-      GetUrlParameterNumber(ctx, ref data, "startrecord", "startRecord", 1, 1, out var start, Template_Error_Number);      
-      GetUrlParameterNumber(ctx, ref data, "maximumrecords", "maximumRecords", 10, 0, _maxRecords, out var maximum, Template_Error_Number);
-
-      // URL parameters are specified but meaningless. In version 1.2 they are used for the scan - which is not implemented in 2.0. But the parameters must be checked according to the specification.
-      GetUrlParameterNumber(ctx, ref data, "responseposition", "responsePosition", start, 1, out start, Template_Error_ScanNumber); // startRecord = responsePosition
-      GetUrlParameterNumber(ctx, ref data, "maximumterms", "maximumTerms", maximum, 0, _maxRecords, out maximum, Template_Error_ScanNumber); // maximumRecords = maximumTerms
-
-      if (data.ContainsKey("query"))
-        ExecuteQuery(ctx, data["query"], start, maximum);
-      else
-        ctx.Response.Send(DefaultRouteResponse, _mime);
-    }
-
-    private void ExecuteQuery(HttpContext ctx, string query, int start, int maximum)
-    {
-      try
-      {
-        SearchResponse result = Search.Send(query, start, maximum);
-
-        if (result?.Hits == null || result.Hits.Length == 0)
+        if (start > 1 && start > result?.EstimatedTotalHits)
         {
-          if (start > 1 && start > result?.EstimatedTotalHits)
-          {
-            ctx.Response.Send(Error_OutOfRange, _mime);
-            return;
-          }
-
-          ctx.Response.Send(EmptyResult.Replace("{{query}}", query), _mime);
+          ctx.Response.Send(Error_OutOfRange, _mime);
           return;
         }
 
-        var stb = new StringBuilder();
-
-        stb.Append(Template_Response_01);
-        stb.Append(result.EstimatedTotalHits.ToString());
-        stb.Append(Template_Response_02);
-
-        for (int i = 0; i < result.Hits.Length; i++)
-          stb.Append(Template_Response_03.Replace("{{id}}", result.Hits[i].Formatted.Id.ToString()).Replace("{{url}}", result.Hits[i].Formatted.Url).Replace("{{hit}}", result.Hits[i].Formatted.Text).Replace("{{p}}", (result.Offset + i).ToString()));
-
-        stb.Append(Template_Response_04);
-        stb.Append(Template_Response_05.Replace("{{query}}", query).Replace("{{start}}", (result.Offset + 1).ToString()).Replace("{{offset}}", start.ToString()).Replace("{{max}}", result.EstimatedTotalHits.ToString()));
-
-        ctx.Response.Send(stb.ToString(), _mime);
+        ctx.Response.Send(EmptyResult.Replace("{{query}}", query), _mime);
+        return;
       }
-      catch (TypeLoadException)
-      {
-        ctx.Response.Send(Error_QueryParser);
-      }
-      catch
-      {
-        ctx.Response.Send(System.Net.HttpStatusCode.InternalServerError);
-      }
+
+      var stb = new StringBuilder();
+
+      stb.Append(Template_Response_01);
+      stb.Append(result.EstimatedTotalHits.ToString());
+      stb.Append(Template_Response_02);
+
+      for (var i = 0; i < result.Hits.Length; i++)
+        stb.Append(Template_Response_03.Replace("{{id}}", result.Hits[i].Formatted.Id)
+          .Replace("{{url}}", result.Hits[i].Formatted.Url).Replace("{{hit}}", result.Hits[i].Formatted.Text)
+          .Replace("{{p}}", (result.Offset + i).ToString()));
+
+      stb.Append(Template_Response_04);
+      stb.Append(Template_Response_05.Replace("{{query}}", query).Replace("{{start}}", (result.Offset + 1).ToString())
+        .Replace("{{offset}}", start.ToString()).Replace("{{max}}", result.EstimatedTotalHits.ToString()));
+
+      ctx.Response.Send(stb.ToString(), _mime);
+    }
+    catch (TypeLoadException)
+    {
+      ctx.Response.Send(Error_QueryParser);
+    }
+    catch
+    {
+      ctx.Response.Send(HttpStatusCode.InternalServerError);
     }
   }
 }
