2976728fe22e895fb24abfde645364d289a982ed
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2026-05-05 14:03:46 +0200

    // OpenApi Python

---
+++ TextPlus.OpenApi.Python/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.default_api import DefaultApi
+from openapi_client.api.search_api import SearchApi
+from openapi_client.api.web_api import WebApi
+from openapi_client.api.weblicht_api import WeblichtApi
+
